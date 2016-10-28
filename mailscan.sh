7cd155a85c1b1de2067157ccff2e295b5d817414
Jean Christophe André <progfou@users.noreply.github.com>
2016-10-28 00:30:20 -0400

    Update mailscan.sh
    Révision du code pour une meilleure lisibilité (pas de changement fonctionnel majeur).

--- mailscan.sh
+++ mailscan.sh
@@ -9,96 +9,84 @@
 
 #set -x
 
-premier_scan="/var/log/clamav/premier_mailscan.log"
-rep_virus="/var/tmp/VIRUS/"
-dir_scan="/var/vmail/"
 notification="calin.dordia@ro.auf.org"
-log_file="/var/log/clamav/hebdo_clamscan.log"
+notif_prefix="ClamAV Log :"
 
-for dir in `ls $dir_scan`;
+dir_scan="/var/vmail/"
+rep_virus="/var/tmp/VIRUS/"
+premier_scan_base="/var/log/clamav/premier_mailscan.log"
+log_base="/var/log/clamav/hebdo_clamscan.log"
 
+# date du jour
+today="`date +%Y-%m-%d`"
+
+# s'assurer que le dossier de quarantaine existe
+mkdir -p "$rep_virus"
+
+# parcourir les domaines de courriels
+for domain in `ls -1 "$dir_scan"`
 do
+  # parcourir les boîtes aux lettres du domaine
+  for dir in `ls -1 "$dir_scan/$domain"`
+  do
+    #DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
+    #echo "Start le scan hebdomadaire du repertoire "$S".
+    #Le scan volume est "$DIRSIZE".";
 
-    for subdir in `ls $dir_scan/$dir`;
+    # fichiers de travail pour ce dossier
+    premier_scan_file="$premier_scan_base $dir"
+    log_file="$log_base $dir"
 
-    do
+    if [ ! -f "$premier_scan_file" ]
+    then # La premiere analyse
+      clamscan -i -l "$premier_scan_file" -r --move="$rep_virus" "$dir_scan"
 
-$(#for S in ${dir_scan}; do 
-#DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
-
-#echo "Start le scan hebdomadaire du repertoire "$S".
-# Le scan volume est "$DIRSIZE".";
-
-# La premiere analyse
-
-if [ ! -d "$rep_virus" ] 
-
-then mkdir "$rep_virus" 
-
-fi
-
-if [ ! -f  "$premier_scan $dir" ] 
-
-then
-
- clamscan -i -l "$premier_scan $dir" -r --move="$rep_virus" "$dir_scan"
-
-### Verifier si le fichier log existe et envoyer par e-mail
-
-VN=$(tail "$premier_scan $dir"|grep Infected|cut -d" " -f3)
-
-if [ "$VN" -ne "0" ]
-
-then    
-    mail -s "ClamAV Log du `date +%d-%m-%Y`" "$notification" < "$premier_scan $dir"
+      # Verifier si le fichier log existe et envoyer par e-mail
+      VN="`tail "$premier_scan_file" | grep Infected | cut -d" " -f3`"
+      if [ "$VN" != "0" ]
+      then
+        cat "$premier_scan_file" | mail -s "$notif_prefix $VN virus" "$notification"
 	echo "Send OK"
-#	cp "$premier_scan" "ClamAV_log`date +%d-%m-%Y`
-	cp "$premier_scan $dir" "$log_file $dir"
-#    echo "" > "$premier_scan"
-else
-    echo "Aucun virus trouve" | mail -s "Aucun Virus ClamAV `date +%d-%m-%Y`" "$notification"
-#    echo "Une erreur de scan envoyee par courriel"
-fi
+        #cp "$premier_scan" "ClamAV_log`date +%d-%m-%Y`
+	cp -af "$premier_scan_file" "$log_file $dir"
+        #echo "" > "$premier_scan"
+      else
+        echo "Aucun virus trouve" | mail -s "$notif_prefix aucun virus" "$notification"
+        #echo "Une erreur de scan envoyee par courriel"
+      fi
 
-# l'analyse hebdomadaire 
+    else # l'analyse hebdomadaire 
+      # creation du fichier temporaire pour la liste de noveaux fichiers 
+      list_file="`mktemp -t clamscan.XXXXXX`" || exit 1
 
-else
-
-# creation du fichier temporaire pour la liste de noveaux fichiers 
-
-list_file=$(mktemp -t clamscan.XXXXXX) || exit 1
-
-# la liste des noveaux fichiers (derniere semaine)
-
-
-if [ -f  "$log_file $dir" ]
-then
+      # la liste des noveaux fichiers (derniere semaine)
+      if [ -f  "$log_file" ]
+      then
         # utiliser les fichiers le plus nouveaux que le fichier temporaire de logs
-        find "$dir_scan" -type f -cnewer "$log_file $dir" -fprint "$list_file"
-else
+        find "$dir" -type f -cnewer "$log_file" -fprint "$list_file"
+      else
         # identifier les messages du derniers 7 jours
-        find "$dir_scan" -type f -ctime 7 -fprint "$list_file"
-fi
+        find "$dir" -type f -ctime 7 -fprint "$list_file"
+      fi
 
-if [ -s "$list_file" ]
-then
+      if [ -s "$list_file" ]
+      then
         # Scan les fichiers et les deplace dans un rep. des  infectes.
-        clamscan -i -f "$list_file" --move="$rep_virus" > "$log_file $dir"
-
+        clamscan -i -f "$list_file" --move="$rep_virus" > "$log_file"
         # envoyer un courriel d'alerte
-        if [ `cat "$log_file $dir" | grep Infected | grep -v 0 | wc -l` != 0 ]
+        VN="`cat "$log_file" | grep Infected | grep -v 0 | wc -l`"
+        if [ "$VN" != "0" ]
         then
-                HOSTNAME=`hostname`
-                echo "$(egrep "FOUND" "$log_file $dir") $dir" | mail -s "Nouvelle parution des viruses on $HOSTNAME"   "$notification"
+          echo "`grep "FOUND" "$log_file"` $dir" | mail -s "$notif_prefix nouveau(x) virus sur `hostname`" "$notification"
         fi
-else
+      else
         # elenver la liste vide, pas d'information relevante
         rm -f "$list_file"
-fi
-fi
-);
+      fi
+    fi
+
+  done
 done
-done
-exit
 
 #set +x
+exit 0
