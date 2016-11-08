7626f43ee01d80bfcc29772d4606fa43d493b000
Calin DORDEA <calin.dordia@auf.org>
2016-11-08 22:54:00 +0200

    nouvelle version avec la quarantine dans le BAL

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mailscan
-Le script propose un scan periodique (hebdomadaire) des boites aux lettres (BAL) en vue d’éliminer les virus restant qui, pour certains raisons, n’ont pas étés détectes à l’arrivée. 
+Le script propose un scan periodique (hebdomadaire) des boites aux lettres (BAL) en vue d'éliminer les virus restant qui, pour certains raisons, 'ont pas étés détectes 'arrivée. 
 Pour éviter le chargement inutile du serveur par un scan complet des anciens messages (parfois les BAL des utilisateurs ont des dizaines de GO) le script exécute un premier scan complet et après il vérifie seulement les messages de derniers 2 mois.
 	 
 Pour le bon fonctionnement du script le service de boîte aux lettres doit être configuré selon les normes recommandés (https://wiki.auf.org/wikiteki/Dovecot). Pour des configurations différentes il faudra adapter les paramètres du script.  
--- mailscan.sh
+++ mailscan.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 ### BEGIN INIT INFO
-# Provides:          ClamAv
-# Short-Description: Un scan supplementaire anti-virus 
-# Description:       Efectue des scan periodiques sur les BAL avec ClamAV et envoie des messages avec les logs.
+# v1 - QUARANTINES DANS LES BAL
+# Besoin:          ClamAv
+# Description: Un scan supplementaire anti-virus pour les BAL des utilisateurs
+# Description: Efectue des scan sur les BAL avec ClamAV et envoie des messages avec les logs.
+# Description: Il peut etre instale dans /etc/cron/weekly
 ### END INIT INFO
 
-#set -x
+#trap "pkill -f 'sleep 1h'" INT
+#trap "set +x; sleep 1h; set -x" DEBUG
+
 
 notification="calin.dordia@ro.auf.org"
 notif_prefix="ClamAV Log :"
-
 dir_scan="/var/vmail/"
 rep_virus="/var/tmp/VIRUS/"
-premier_scan_base="/var/log/clamav/premier_mailscan.log"
-log_base="/var/log/clamav/hebdo_clamscan.log"
+premier_scan_base="/var/log/clamav/premier_mailscan_"
+log_base="/var/log/clamav/hebdo_clamscan_"
+log_dir="/var/log/clamav/"
 
 # date du jour
-today="`date +%Y-%m-%d`"
+#today="`date +%Y-%m-%d`"
 
 # s'assurer que le dossier de quarantaine existe
 mkdir -p "$rep_virus"
@@ -27,19 +31,23 @@ mkdir -p "$rep_virus"
 for domain in `ls -1 "$dir_scan"`
 do
   # parcourir les boîtes aux lettres du domaine
-  for dir in `ls -1 "$dir_scan/$domain"`
+  for bal in `ls -1 "$dir_scan$domain"`
   do
     #DIRSIZE=$(du -sh "$S" 2>/dev/null | cut -f1);
     #echo "Start le scan hebdomadaire du repertoire "$S".
     #Le scan volume est "$DIRSIZE".";
 
+    # s'assurer que le dossier de quarantaine existe dans chaque BAL
+    mkdir -p "$dir_scan$domain"/"$bal"/"".VirusMail""
+     
     # fichiers de travail pour ce dossier
-    premier_scan_file="$premier_scan_base $dir"
-    log_file="$log_base $dir"
+    premier_scan_file="$premier_scan_base$bal"
+    log_file="$log_base$bal"
+    quarantine="$dir_scan$domain"/"$bal"/"".VirusMail""
 
     if [ ! -f "$premier_scan_file" ]
     then # La premiere analyse
-      clamscan -i -l "$premier_scan_file" -r --move="$rep_virus" "$dir_scan"
+      clamscan -i  --exclude-dir="$quarantine" -l "$premier_scan_file" -r --move="$quarantine"   "$dir_scan"
 
       # Verifier si le fichier log existe et envoyer par e-mail
       VN="`tail "$premier_scan_file" | grep Infected | cut -d" " -f3`"
@@ -48,7 +56,7 @@ do
         cat "$premier_scan_file" | mail -s "$notif_prefix $VN virus" "$notification"
 	echo "Send OK"
         #cp "$premier_scan" "ClamAV_log`date +%d-%m-%Y`
-	cp -af "$premier_scan_file" "$log_file $dir"
+	cp -af "$premier_scan_file" "$log_file"
         #echo "" > "$premier_scan"
       else
         echo "Aucun virus trouve" | mail -s "$notif_prefix aucun virus" "$notification"
@@ -60,31 +68,32 @@ do
       list_file="`mktemp -t clamscan.XXXXXX`" || exit 1
 
       # la liste des noveaux fichiers (derniere semaine)
-      if [ -f  "$log_file" ]
-      then
+      #if [ -f  "$log_file" ]
+      #then
         # utiliser les fichiers le plus nouveaux que le fichier temporaire de logs
-        find "$dir" -type f -cnewer "$log_file" -fprint "$list_file"
-      else
+        #find "$dir_scan$domain/$bal" -type f -cnewer "$log_file" -fprint "$list_file"
+      #else
         # identifier les messages du derniers 7 jours
-        find "$dir" -type f -ctime 7 -fprint "$list_file"
-      fi
+        find "$dir_scan$domain/$bal" -type f -mtime -60 -fprint "$list_file"
+      #fi
 
       if [ -s "$list_file" ]
       then
         # Scan les fichiers et les deplace dans un rep. des  infectes.
-        clamscan -i -f "$list_file" --move="$rep_virus" > "$log_file"
+        clamscan -i --exclude-dir="$quarantine"  -f "$list_file"  -r  --move="$quarantine"  > "$log_file"
         # envoyer un courriel d'alerte
         VN="`cat "$log_file" | grep Infected | grep -v 0 | wc -l`"
         if [ "$VN" != "0" ]
         then
-          echo "`grep "FOUND" "$log_file"` $dir" | mail -s "$notif_prefix nouveau(x) virus sur `hostname`" "$notification"
+          echo "`grep "FOUND" "$log_file"` $bal" | mail -s "$notif_prefix nouveau(x) virus sur `hostname`" "$notification"
         fi
       else
         # elenver la liste vide, pas d'information relevante
         rm -f "$list_file"
       fi
     fi
-
+    #changer le propritaire de la quarantine pour que l'utilisateur puisse y operer
+    chown -R "vmail":"vmail" "$quarantine"
   done
 done
 
